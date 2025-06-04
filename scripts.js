e29f90fa4e93f1c09d71b9a872330a3150391287
Pichaku123 <pribisht785@gmail.com>
2025-06-04 12:41:20 +0530

    added resizing, made the code a bit cleaner

--- scripts.js
+++ scripts.js
@@ -1,19 +1,35 @@
-const n=16;
+let n=50;
+const canvasWidth=960;
+let pixelWidth=960/n;
+
+const resize=document.querySelector("#resize");
 const container=document.querySelector("#container");
-//n rows and n column
-//let's make the container equal to 16*16 of the inner divs
-//abhi agar inner div=20*20, so outer=320*320;
-for(let i=0; i<n*n; i++)
-{
-    const temp=document.createElement("div"); //this is the rows
-    temp.classList.add("pixel");
-    //added the eventlistener while creating the divs itself to save us trouble.
-    temp.addEventListener("mouseover", () => {
-        console.log(`colored ${i}`);
-    });
-    container.appendChild(temp);
+
+grid(n, pixelWidth);
+
+resize.addEventListener("click", () => {
+    n=Number(prompt("Add grid size- "));
+    pixelWidth=960/n;
+    grid(n, pixelWidth);
+})
+
+
+function grid(n, pixelWidth){
+    container.innerHTML="";
+    for(let i=0; i<n*n; i++)
+        {
+            const temp=document.createElement("div"); //this is the rows
+            temp.classList.add("pixel");
+            temp.style.cssText=`width:${pixelWidth}px; height:${pixelWidth}px; box-sizing: border-box; border:1px solid black;`
+            //added the eventlistener while creating the divs itself to save us trouble.
+            temp.addEventListener("mouseover", (e) => {
+                console.log(`colored ${i}`);
+                temp.style.backgroundColor="lightblue";
+            });
+            container.appendChild(temp);
+        }
 }
-const pixels=document.querySelectorAll(".pixel");
+
 // container.addEventListener("mouseover", (e) =>{
 //     e.target.backgroundColor="lightblue";
     
