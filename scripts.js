7b77ca7ecdc45e1166dce3c283ff8ee30233f235
Pichaku123 <pribisht785@gmail.com>
2025-06-04 12:46:51 +0530

    resize only takes <=99 as i/p + some code cleanup

--- scripts.js
+++ scripts.js
@@ -5,10 +5,16 @@ let pixelWidth=960/n;
 const resize=document.querySelector("#resize");
 const container=document.querySelector("#container");
 
-grid(n, pixelWidth);
+grid(n, pixelWidth);    //just to create a default 50x50 grid
 
 resize.addEventListener("click", () => {
-    n=Number(prompt("Add grid size- "));
+    n=Number(prompt("Add grid size(<99)- "));
+    
+    while(n>99)
+    {
+        alert("Please enter width <99.");
+        n=Number(prompt("Add grid size(<99)- "));
+    }
     pixelWidth=960/n;
     grid(n, pixelWidth);
 })
@@ -18,7 +24,7 @@ function grid(n, pixelWidth){
     container.innerHTML="";
     for(let i=0; i<n*n; i++)
         {
-            const temp=document.createElement("div"); //this is the rows
+            const temp=document.createElement("div");
             temp.classList.add("pixel");
             temp.style.cssText=`width:${pixelWidth}px; height:${pixelWidth}px; box-sizing: border-box; border:1px solid black;`
             //added the eventlistener while creating the divs itself to save us trouble.
@@ -30,8 +36,3 @@ function grid(n, pixelWidth){
         }
 }
 
-// container.addEventListener("mouseover", (e) =>{
-//     e.target.backgroundColor="lightblue";
-    
-//     console.log("colored");
-// })
